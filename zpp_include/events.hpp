4852686e33762f4b4fc010ee31bf222a161d30f0
tobias.moullet <tobias.moullet@hefr.ch>
2025-10-21 16:11:56 +0200

    add: events wrapper class

---
+++ zpp_include/events.hpp
@@ -0,0 +1,59 @@
+// Copyright 2025 Haute école d'ingénierie et d'architecture de Fribourg
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/****************************************************************************
+ * @file events.hpp
+ * @author Serge Ayer <serge.ayer@hefr.ch>
+ *
+ * @brief CPP class declaration for wrapping zephyr os Events
+ *
+ * @date 2025-08-31
+ * @version 1.0.0
+ ***************************************************************************/
+
+#pragma once
+
+#if CONFIG_EVENTS == 1
+
+// zephyr
+#include <zephyr/kernel.h>
+
+// stl
+#include <chrono>
+
+namespace zpp_lib {
+
+class Events {
+public:
+    /** 
+     * Create and Initialize a Events object
+     */
+    Events(); 
+
+    void set(uint32_t event_flag);
+
+    void wait_any(uint32_t events_flags);
+
+    void try_wait_any_for(
+      const std::chrono::milliseconds& timeout, uint32_t events_flags);
+
+    ~Events();
+
+private:
+    struct k_event _event_obj;
+};
+
+}  // namespace zpp_lib
+
+#endif  // CONFIG_EVENTS == 1
