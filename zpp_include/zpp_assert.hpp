2b6c0d7598ca1d7bea0e02ff89f8e251dfebca37
SergeAyer <Serge.Ayer@hefr.ch>
2026-09-23 17:05:31 +0200

    Precommit fixes

--- zpp_include/zpp_assert.hpp
+++ zpp_include/zpp_assert.hpp
@@ -37,15 +37,15 @@
 #define ZPP_ASSERT_EVAL(...) __ASSERT_EVAL(__VA_ARGS__)  // NOLINT
 // NOLINTNEXTLINE(readability/nolint)
 // NOLINTEND(cppcoreguidelines-avoid-do-while)
-#else // !defined(ZPP_CLANG_TIDY)
+#else                                                    // !defined(ZPP_CLANG_TIDY)
 #define ZPP_ASSERT(...) __ASSERT(__VA_ARGS__)            // NOLINT
 #define ZPP_ASSERT_EVAL(...) __ASSERT_EVAL(__VA_ARGS__)  // NOLINT
 // NOLINTNEXTLINE(readability/nolint)
 // NOLINTEND(cppcoreguidelines-avoid-do-while)
-#endif // ZPP_CLANG_TIDY
+#endif                                                   // ZPP_CLANG_TIDY
 
 #if CONFIG_TEST
 #define zpp_zassert_true(...) zassert_true(__VA_ARGS__)      // NOLINT
 #define zpp_zassert_equal(...) zassert_equal(__VA_ARGS__)    // NOLINT
 #define zpp_zassert_within(...) zassert_within(__VA_ARGS__)  // NOLINT
-#endif  // CONFIG_TEST
+#endif                                                       // CONFIG_TEST
--- zpp_include/zpp_log.hpp
+++ zpp_include/zpp_log.hpp
@@ -37,11 +37,11 @@
 #define ZPP_LOG_DBG(...)
 #define ZPP_LOG_WRN(...)
 #define ZPP_LOG_ERR(...)
-#else // !defined(ZPP_CLANG_TIDY)
+#else                                          // !defined(ZPP_CLANG_TIDY)
 #define ZPP_LOG_INF(...) LOG_INF(__VA_ARGS__)  // NOLINT
 #define ZPP_LOG_DBG(...) LOG_DBG(__VA_ARGS__)  // NOLINT
 #define ZPP_LOG_WRN(...) LOG_WRN(__VA_ARGS__)  // NOLINT
 #define ZPP_LOG_ERR(...) LOG_ERR(__VA_ARGS__)  // NOLINT
 // NOLINTNEXTLINE(readability/nolint) -- false positive
 // NOLINTEND(cppcoreguidelines-pro-type-vararg,cppcoreguidelines-pro-bounds-array-to-pointer-decay)
-#endif // ZPP_CLANG_TIDY
+#endif                                         // ZPP_CLANG_TIDY
